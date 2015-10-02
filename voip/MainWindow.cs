7ce4c9ff64c0c80c4eb4b38f7d981e83ac70644e
argha <nargha03@gmail.com>
2015-10-02 19:08:18 +0530

    initialising git

---
+++ voip/MainWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+
+	protected void OnBtnCallClicked (object sender, EventArgs e)
+	{
+		string ip = TextBoxIP.Text;
+		string get_textview_value = TextViewStatus.Buffer.Text;
+
+
+		TextViewStatus.Buffer.Text = get_textview_value + "Call Connecting to " + ip + "\n";
+	}
+
+
+	protected void OnBtnDisconnectClicked (object sender, EventArgs e)
+	{
+		string get_textview_value = TextViewStatus.Buffer.Text;
+		TextViewStatus.Buffer.Text = get_textview_value + "Call Disconnected" + "\n";
+	}
+
+	protected void OnBtnClearStatusClicked (object sender, EventArgs e)
+	{
+		TextViewStatus.Buffer.Text = "";
+	}
+}
